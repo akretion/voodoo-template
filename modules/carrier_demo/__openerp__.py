c0b0e6eee508f8ec589958c8370d048b264d4baa
David Beal <david.beal@akretion.com>
2015-11-19 11:13:12 +0100

    [IMP] add gls

---
+++ modules/carrier_demo/__openerp__.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+{
+    'name': "Carriers demo",
+    'summary': """ ColiPoste & GLS Test """,
+    'description': """
+    """,
+    'author': "Akretion",
+    'category': 'demo',
+    'version': '0.1',
+    # any module necessary for this one to work correctly
+    'depends': [
+        'delivery_carrier_label_colissimo',
+        'delivery_carrier_label_so_colissimo',
+        'delivery_carrier_label_gls',
+        'document',
+    ],
+    'data': [],
+    'demo': [],
+    'auto_install': False,
+    'tests': [
+    ],
+}
