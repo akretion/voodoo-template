9b063db9e5dabf21e71a6a3df0c3afb160d88ce7
David Beal <david.beal@akretion.com>
2016-02-04 13:12:00 +0100

    [ADD] module connector flow 

---
+++ modules/custom/__openerp__.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+# © 2016 David BEAL @ Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'custom',
+    'version': '1.0',
+    'category': 'custom',
+    'sequence': 10,
+    'summary': "",
+    'description': """
+custom of the client
+    """,
+    'author': 'Akretion',
+    'website': 'http://www.akretion.com',
+    'depends': [
+        'connector_flow_example_product',
+        'admin_technical_features',
+        ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'images': [
+    ],
+    'css': [
+    ],
+    'js': [
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+}
